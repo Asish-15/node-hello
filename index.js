3e739580a1786003ef00441314be4743ce83bd13
MULAGURI ASISH TONY <100105666+Asish-15@users.noreply.github.com>
2024-02-06 18:05:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+var http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello, You have successfully launched your Nodejs App on EC2'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
